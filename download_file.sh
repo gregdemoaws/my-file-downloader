1ed2eecc579e0f6d2b59f1434a5ce73949c91284
demogregaws <demogregaws@gmail.com>
2023-01-14 16:38:55 +0100

    got rid of the auto extension feature

--- download_file.sh
+++ download_file.sh
@@ -10,17 +10,8 @@ download_file() {
     # Download the file using curl, with error handling
     curl --fail --user-agent "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0" -o "$filename" "$url_to_download" || { echo "Error: Failed to download file $url" ; return 1; }
 
-    # Get the file type ("image/png" for instance)
-    file_type=$(file -b --mime-type "${filename}")
-
-    # Extract the file extension
-    extension=$(echo "$file_type" | awk -F/ '{print $2}')
-
-    # Rename the file by appending the correct file extension
-    mv "$filename" "$filename.$extension"
-
     echo "end of download_file succeeded!"
 }
 
-# Call the function with the URL of the file and the name of the local file to create (without any extension)
+# Call the function with the URL of the file and the name of the local file to create
 download_file $1 $2
