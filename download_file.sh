892abbc7a0973848f20be1b78882fdcf82033a6d
demogregaws <demogregaws@gmail.com>
2023-01-14 16:55:50 +0100

    Turned the filename to a path to avoid a dependency

--- download_file.sh
+++ download_file.sh
@@ -2,16 +2,16 @@
 
 download_file() {
     local url_to_download=$1
-    local filename=$2
+    local output_file_path=$2
     echo ""
     echo "url_to_download: ${url_to_download}"; echo ""
-    echo "filename: ${filename}"; echo ""
+    echo "output_file_path: ${output_file_path}"; echo ""
 
     # Download the file using curl, with error handling
-    curl --fail --user-agent "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0" -o "$filename" "$url_to_download" || { echo "Error: Failed to download file $url" ; return 1; }
+    curl --fail --user-agent "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0" -o "$output_file_path" "$url_to_download" || { echo "Error: Failed to download file $url" ; return 1; }
 
     echo "end of download_file succeeded!"
 }
 
-# Call the function with the URL of the file and the name of the local file to create
+# Call the function with the URL of the file and the output file path to use
 download_file $1 $2
